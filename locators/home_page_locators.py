e9bd65a286505835206677ad38ba97fec0fd1b0a
Paulina Kujawa <paulina.kujawa1991@gmail.com>
2022-07-05 20:03:06 +0200

    Project test refactor

---
+++ locators/home_page_locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators:
+    arrow_head_button = (By.XPATH, "//div[@data-dropdown-id='user_dropdown']/button")
+    login_button = (By.XPATH, '//a[@data-analytics-click-value="login-button"]')
